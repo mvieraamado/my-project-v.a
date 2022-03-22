d3a2466c20d242684c1c00ba61a796fba776c0d6
Mariam Viera <mariamviera7@gmail.com>
2022-03-21 23:16:55 -0300

    feat: application called class

---
+++ class/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Manicurist(models.Model):
+    name = models.CharField(max_length=20)
+    lastName = models.CharField(max_length=20)
+    email = models.EmailField()
+    def __str__(self):
+        return f"Name: {self.name} Last name: {self.lastName}"
+
+class Client(models.Model):
+    name = models.CharField(max_length=20)
+    lastName = models.CharField(max_length=20)
+    email = models.EmailField()
+    job = models.CharField(max_length=40)
+    def __str__(self):
+        return f"Name: {self.name} Last name: {self.lastName} Job: {self.job}"
+
+class Turn(models.Model):
+    date = models.DateTimeField()
+    def __str__(self):
+        return f"Turn: {self.date}"

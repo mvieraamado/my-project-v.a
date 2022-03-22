d4554c55b8e5dd97a7c546363761fc990ac771d6
Mariam Viera <mariamviera7@gmail.com>
2022-03-21 21:40:30 -0300

    feat: a template has been added

--- index/views.py
+++ index/views.py
@@ -1,10 +1,4 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-
-def index(request):
-    return HttpResponse('<h1>Bienvenido a mi pagina de django</h1>')
-
-def plantilla(request):
-    return HttpResponse('')
+def home(request):
+    return render(request, 'index/index.html', {})

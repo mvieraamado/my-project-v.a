277be00ebcbcd50e4a4db1a992cff9ffde64a053
Mariam Viera <mariamviera7@gmail.com>
2022-03-22 19:19:00 -0300

    feat: I have created a new application, in it I have added a template and created the models

--- clase/admin.py
+++ clase/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from clase.models import Client_n, Manicurist, Turn
+
 # Register your models here.
+admin.register(Manicurist)
+admin.register(Client_n)
+admin.register(Turn)
--- clase/views.py
+++ clase/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def searchManicurist(request):
+    return render(request, 'clase/search_manicurist.html', {})

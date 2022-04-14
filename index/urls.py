75e036a6a5d8ef46d473dfd89cd8edc3db6b1997
Mariam Viera <mariamviera7@gmail.com>
2022-04-13 23:33:24 -0300

    feat: login, user editing and registration created

--- index/urls.py
+++ index/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path 
-from .views import home, photos
+from .views import home, photos, my_login, user_register, user_edit
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', home, name='home'),
-    path('photos/', photos, name='photos')
+    path('photos/', photos, name='photos'),
+    path('login/', my_login, name='login'),
+    path('logout/', LogoutView.as_view(template_name='index/index.html'), name='logout'),
+    path('register/', user_register, name='register'),
+    path('edit/', user_edit, name='edit')
 ]
 

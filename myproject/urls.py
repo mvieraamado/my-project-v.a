65a95d1d6888d9832fe9f7e9619933b13d98f68f
Mariam Viera <mariamviera7@gmail.com>
2022-04-16 22:03:23 -0300

    feat: Changes have been added to display database images

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+from myproject.settings import MEDIA_ROOT
 
 urlpatterns = [
     path('', include("index.urls")),
     path('', include("clase.urls")),
+    path('', include("accounts.urls")),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)

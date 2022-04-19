2e3a550a9127a6a7d7997db7e00fb2a6b2448a98
Mariam Viera <mariamviera7@gmail.com>
2022-04-19 01:00:59 -0300

    feat: avatar information

--- index/urls.py
+++ index/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path 
-from .views import home, photos, my_login, user_register, user_edit
-from django.contrib.auth.views import LogoutView
+from .views import home, about
 
 urlpatterns = [
     path('', home, name='home'),
-    path('photos/', photos, name='photos'),
-    path('login/', my_login, name='login'),
-    path('logout/', LogoutView.as_view(template_name='index/index.html'), name='logout'),
-    path('register/', user_register, name='register'),
-    path('edit/', user_edit, name='edit')
+    path('about/', about, name='about')
 ]
 
--- index/views.py
+++ index/views.py
@@ -1,81 +1,20 @@
 from django.shortcuts import render
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from index.forms import CreateAUser, EditAUser
-from django.contrib.auth.decorators import login_required #para funciones
-# from django.contrib.auth.mixins import LoginRequiredMixin   -->  se utiliza para clases basadas en vistas
+from accounts.views import search_avatar
+
+from clase.models import BlogPosts
+# from accounts.views import search_avatar
 
 def home(request):
-    return render(request, 'index/index.html', {})
+    if request.user.is_authenticated:
+        return render(request, 'index/home.html', {'search_avatar':search_avatar(request.user)})
+    else:
+        return render(request, 'index/home.html', {})
 
-@login_required
-def photos(request):
-    return render(request, 'index/photos.html', {})
-
-def my_login(request):
-    if request.method == 'POST':
-        form_login= AuthenticationForm(request, data=request.POST)
-        if form_login.is_valid():
-            username = form_login.cleaned_data['username']
-            password = form_login.cleaned_data['password']
-            
-            user = authenticate(username = username, password = password)
-            
-            if user is not None:
-                login(request, user)
-                return render(request, 'index/index.html', {})
-            else:
-                return render(request, 'index/login.html', {'form_login': form_login, 'mensaje': 'No se encuentra autenticado'})
-                
-        else :
-            return render(request, 'index/login.html', {'form_login': form_login, 'mensaje': 'Datos incorrectos'})
-    else: 
-        form_login= AuthenticationForm()
-        return render (request, 'index/login.html', {'form_login': form_login, 'mensaje': ''})
-
-def user_register (request):
-    if request.method == 'POST':
-        form_register = CreateAUser(request.POST)
-
-        if form_register.is_valid():
-            username = form_register.cleaned_data['username']
-            
-            form_register.save()
-            return render(request, 'index/index.html', {'mensaje': f'Se creo exitosamente {username}'})
-        else:
-            return render(request, 'index/register.html', {'form_register': form_register})
+def about(request):
+    if request.user.is_authenticated:
+        return render(request, 'index/about.html', {'search_avatar':search_avatar(request.user)})
+    else:
+        return render(request, 'index/about.html', {})
     
-    form_register = CreateAUser()
-    return render(request, 'index/register.html', {'form_register': form_register, 'mensaje': ''})
-
-@login_required
-def user_edit (request):
-    if request.method == 'POST':
-        form_edit = EditAUser(request.POST)
-        
-        if form_edit.is_valid():
-            data = form_edit.cleaned_data
-            
-            loggued_in_user = request.user
-            loggued_in_user.email = data.get('email')
-            loggued_in_user.first_name = data.get('first_name', '')
-            loggued_in_user.last_name = data.get('last_name', '')
-            
-            if data.get('password1') == data.get('password2') and len(data.get('password2')) > 8:
-                loggued_in_user.set_password(data.get('password1'))
-                
-            loggued_in_user.save()
-            return render(request, 'index/index.html', {})
-        else:
-            return render(request, 'index/edit_user.html', {'form_edit': form_edit})
-    form_edit = EditAUser(
-        initial={
-            'username' : request.user.username,
-            'first_name' : request.user.first_name,
-            'last_name' : request.user.last_name,
-            'email' : request.user.email
-        }
-    )
-    return render(request, 'index/edit_user.html', {'form_edit': form_edit})
 
     
